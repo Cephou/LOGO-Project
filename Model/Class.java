344bda043a854922855994ade354e405c204684e
Severin <lhommeletseverin@gmail.com>
2017-03-29 15:04:09 +0200

    attributs and constructeur

--- Model/Class.java
+++ Model/Class.java
@@ -4,9 +4,21 @@
 package Model;
 
 /**
- * @author Laurent
+ * @author grp7
  *
  */
-public class Classe {
+public class Class {
+	
+	private String className;
+	private Teacher teacher;
+	private ArrayList<Child> childList;
+	
+	public Classe(String newClassName, Teacher newTeacher)
+	{
+		className = newClassName;
+		teqcher = newTeacher;
+		childList = new ArrayList<Child>();		
+	}
+	
 
 }
