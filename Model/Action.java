422db6c1642ffd0a279608e01dffc238803b5b26
Cephou <laurent.noyon@gmail.com>
2017-03-29 16:01:31 +0200

    advanced action

--- Model/Action.java
+++ Model/Action.java
@@ -1,15 +1,50 @@
 package Model;
 
+import Model.Tortue.TortueG;
+
 public class Action {
 	
-	private String command;
+	private TortueG turtleType;
+	private String command; // La commande de l'action (avancer, tourner etc)
 	
-	public Action(String pcommand) {
-		command = pcommand;
+	public Action(String pcommand, TortueG pturtle) {
+		setCommand(pcommand);
+		setTurtleType(pturtle);
+	}
+
+	/*
+	 * Dessine l'action sur le canvas tortue à l'aide de la commande et le type de tortue
+	 */
+	public void drawTurtle() {
+		
 	}
 	
-	public void drawTurtle(String pcommand) {
-		
+	/**
+	 * @return the turtleType
+	 */
+	public TortueG getTurtleType() {
+		return turtleType;
+	}
+
+	/**
+	 * @param turtleType the turtleType to set
+	 */
+	public void setTurtleType(TortueG turtleType) {
+		this.turtleType = turtleType;
+	}
+
+	/**
+	 * @return the command
+	 */
+	public String getCommand() {
+		return command;
+	}
+
+	/**
+	 * @param command the command to set
+	 */
+	public void setCommand(String command) {
+		this.command = command;
 	}
 
 }
