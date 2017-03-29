3bfdd02ab7d666a6be7bf152a033786c372bcfb9
SgtMagus <jouannet.pierre@gmail.com>
2017-03-29 16:06:23 +0200

    push classe exercise boooooys

--- Model/Exercise.java
+++ Model/Exercise.java
@@ -1,5 +1,7 @@
 package Model;
 
+import javax.swing.ImageIcon;
+
 /**
  * @author Groupe 7
  * Cette classe décrit l'objet exercice.
@@ -13,6 +15,65 @@ public class Exercise {
 
 	public Exercise(String name, String instruction, String description, ImageIcon icon) {
 		this.name = name;
-		this.
+		this.instruction = instruction;
+		this.description = description;
+		this.icon = icon;
 	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the instruction
+	 */
+	public String getInstruction() {
+		return instruction;
+	}
+
+	/**
+	 * @param instruction the instruction to set
+	 */
+	public void setInstruction(String instruction) {
+		this.instruction = instruction;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the icon
+	 */
+	public ImageIcon getIcon() {
+		return icon;
+	}
+
+	/**
+	 * @param icon the icon to set
+	 */
+	public void setIcon(ImageIcon icon) {
+		this.icon = icon;
+	}
+	
 }
