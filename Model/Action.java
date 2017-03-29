4d7fc88e2094d45bf8dc229dd352779a7bfb61a3
Cephou <laurent.noyon@gmail.com>
2017-03-29 15:50:07 +0200

    action début

--- Model/Action.java
+++ Model/Action.java
@@ -1,5 +1,15 @@
 package Model;
 
 public class Action {
+	
+	private String command;
+	
+	public Action(String pcommand) {
+		command = pcommand;
+	}
+	
+	public void drawTurtle(String pcommand) {
+		
+	}
 
 }
