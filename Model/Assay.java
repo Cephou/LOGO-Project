a6cec71e74f1a4d777a3f9a4df80e866ce93fdda
Alexis A <alexis.amiand@gmail.com>
2017-03-29 15:11:52 +0200

    temp commit

--- Model/Assay.java
+++ Model/Assay.java
@@ -11,8 +11,31 @@ package Model;
  */
 public class Assay {
 	
-	ArrayList<Action> listAction;
-	Child child;
-	Exercice exercice;
+	private ArrayList<Action> listAction;
+	private Child child;
+	private Exercice exercice;
+	private String comment;
+	private Grade grade;
 
+	/**
+	 * Constructeur de la classe Tentative
+	 * @param child l'enfait qui effectue la tentative
+	 * @param  exercice l'exercice sur le quel la tentative est exercée
+	 */
+	public Assay(child, exercice){
+		this.child = child;
+		this.exercice = exercice;
+		listAction = new ArrayList<Action>();
+		
+	}
+	
+	public void addAction(Action a){
+		lsitAction.add(a)
+	}
+	
+	public void evaluateAssay(comment, grade){
+		this.comment = comment;
+		this.grade = grade;
+	}
+	
 }
