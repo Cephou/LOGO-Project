fe801e514fa12ed8ee02ffe4effbba5be26a66ef
Wassim <wassim.kerkri@gmail.com>
2017-03-31 14:42:42 +0200

    ExerciseVisualizationView + AssaysListView

--- View/AssaysListView.java
+++ View/AssaysListView.java
@@ -1,13 +1,20 @@
 package View;
 
+import Model.Child;
+import Model.Exercise;
+
 /**
- * Cette classe permet de créer une liste de tentatives.
+ * Cette classe permet de créer une liste de tentatives d'un exercice particulier pour un enfant particulier.
+ * Les données concernant les tentatives sont récupérées à partir d'une base de données.
  */
 public class AssaysListView {
 
     /**
      * Constructeur de la classe AssaysListView.
+     *
+     * @param child est l'enfant qui sélectionne l'exercice
+     * @param exercise est l'exercice sélectionné par l'enfant
      */
-    public AssaysListView() {
+    public AssaysListView(Child child, Exercise exercise) {
     }
 }
--- View/ExerciseVisualizationView.java
+++ View/ExerciseVisualizationView.java
@@ -4,16 +4,17 @@ import Model.Child;
 import Model.Exercise;
 
 /**
- * Cette classe permet, après sélection d'un exercice par un enfant, de pouvoir rejouer l'exercice, accéder à la liste des tentatives réalisées,
- * visualiser les tentativées réalisées, visualiser la solution de l'exercice, lire l'appréciation de l'enseignant,
- * visualiser le statut de l'exercice (à faire, en cours de correction, corrigé).
+ * Cette classe permet, après sélection d'un exercice par un enfant, de pouvoir rejouer l'exercice,
+ * accéder à la liste des tentatives réalisées, visualiser les tentativées réalisées, visualiser la solution de l'exercice,
+ * lire l'appréciation de l'enseignant, visualiser le statut de l'exercice (à faire, en cours de correction, corrigé).
+ * Cette classe construit les différents éléments de l'interface à partir d'autres classes.
  */
 public class ExerciseVisualizationView {
 
     /**
      * Constructeur de la classe ExerciseVisualizationView.
      *
-     * @param child    est l'enfant qui sélectionné l'exercice
+     * @param child    est l'enfant qui sélectionne l'exercice
      * @param exercise est l'exercice sélectionné par l'enfant
      */
     public ExerciseVisualizationView(Child child, Exercise exercise) {
