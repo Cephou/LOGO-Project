98b7efca34a687b184485237e431b602a9e0da6c
Alexis A <alexis.amiand@gmail.com>
2017-03-31 14:49:01 +0200

    child list

--- View/ChildListView.java
+++ View/ChildListView.java
@@ -3,11 +3,26 @@
  */
 package View;
 
+import javax.swing.JPanel;
+import Model.SchoolClass;
+
 /**
- * Vue permettant l'affichage et la selection des enfants d'une classe.
- *La classe est affichée dans une JList
- *La selection d'un enfant permet au professeur de voir les exercices qu'il a réalisé dans le panel ExerciseListView
+ * Vue permettant l'affichage et la selection des enfants d'une classe. La
+ * classe est affichée dans une JList La selection d'un enfant permet au
+ * professeur de voir les exercices qu'il a réalisé dans le panel
+ * ExerciseListView
  */
 public class ChildListView {
+
+	/**
+	 * Constructeur de JList d'eleve.
+	 * Prend unne classe en parametre et genere une liste
+	 * Un seul enfant est clickale a la fois
+	 * un enfant cliqu" est surgné en bleu
+	 */
+	public ChildListView(SchoolClass class) {
+			// Procédure de boucle qui pioche dans la classe choisie pour afficher les enfants.		
+		}	
+}
 
 }
