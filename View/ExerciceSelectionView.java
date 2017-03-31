866a26f380403ac4fc5af1170b2023413590d8b7
Cephou <laurent.noyon@gmail.com>
2017-03-31 14:48:51 +0200

    .

--- View/ExerciceSelectionView.java
+++ View/ExerciceSelectionView.java
@@ -2,7 +2,6 @@ package View;
 
 /*
  * Cette classe affiche la liste des exercices de l'élève d'une part, mais aussi la visualisation d'un exercice au clic
- * 
  */
 
 public class ExerciceSelectionView {
