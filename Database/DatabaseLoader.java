238fc3d3013a74ec99f40749f715435687f4ed4f
Pierre <jouannet.pierre@gmail.com>
2017-03-31 15:55:33 +0200

    Ajout d'exemples de syntaxe JDBC/SQLite

--- Database/DatabaseLoader.java
+++ Database/DatabaseLoader.java
@@ -4,4 +4,27 @@ package Database;
 
 public class DatabaseLoader {
 
+	/*
+	 * Exemple de syntaxe de connexion de BDD JDBC/SQLite :
+	 * 
+	 * 
+	 * import java.sql.*;
+
+	public class SQLiteJDBC
+	{
+	  public static void main( String args[] )
+	  {
+	    Connection c = null;
+	    try {
+	      Class.forName("org.sqlite.JDBC");
+	      c = DriverManager.getConnection("jdbc:sqlite:test.db");
+	    } catch ( Exception e ) {
+	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	      System.exit(0);
+	    }
+	    System.out.println("Opened database successfully");
+	  }
+	}*/
+	
+	
 }
