183572805705a6e80e8eb05a4d33b236310c40d1
Cephou <laurent.noyon@gmail.com>
2017-03-31 14:12:44 +0200

    .

--- Controler/ChildSelection.java
+++ Controler/ChildSelection.java
@@ -1,5 +1,9 @@
 package Controler;
 
+/*
+ * Cette classe est le controleur des boutons issus de ChildSelectionView.
+ * Elle permet de choisir l'enfant et instancie la génération de ExerciceSelectionView, en lui donnant l'enfant.
+ */
 public class ChildSelection {
 
 }
--- View/ExerciceSelectionView.java
+++ View/ExerciceSelectionView.java
@@ -1,5 +1,10 @@
 package View;
 
+/*
+ * Cette classe affiche la liste des exercices de l'élève d'une part, mais aussi la visualisation d'un exercice au clic
+ * 
+ */
+
 public class ExerciceSelectionView {
 
 }
