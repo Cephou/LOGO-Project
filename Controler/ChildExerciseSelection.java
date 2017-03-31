9cb3b8c5dc2ba85bdd40fe3700ffcb2d5f38117f
Cephou <laurent.noyon@gmail.com>
2017-03-31 14:37:41 +0200

    views

---
+++ Controler/ChildExerciseSelection.java
@@ -0,0 +1,9 @@
+package Controler;
+
+/*
+ * Cette classe permet de choisir un exercice. Elle permet d'afficher le module ExerciceSelectionView
+ */
+
+public class ChildExerciseSelection {
+
+}
---
+++ View/ExerciseListView.java
@@ -0,0 +1,11 @@
+package View;
+
+/*
+ * Cette classe permet de visualiser et de choisir un exercice. 
+ * Elle prend en paramètre un élève et affiche la liste d'exercices correspondants.
+ * Chaque exercice est liée à un controleur, ExerciceSelection, qui permet de sélectionner un exercice.
+ */
+
+public class ExerciseListView {
+
+}
