eb14ffb824d00d2ace05008cba4aa83a724a61ad
Alexis A <alexis.amiand@gmail.com>
2017-03-31 15:39:17 +0200

    subclasschild ad  description

--- SubMainChild.java
+++ SubMainChild.java
@@ -1,4 +1,8 @@
 
 public class SubMainChild {
+	/**
+	 * Ce main est appele le layout lorsque l'utilisateuer choisi est un eleve.
+	 * Il permet de sauvergarder les varaibles "session" comme l'eleve couant par exemple.
+	 */
 
 }
