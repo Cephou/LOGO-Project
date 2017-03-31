c72e6840f20c4b6da5a4c1835eb22f422992147e
Alexis A <alexis.amiand@gmail.com>
2017-03-31 14:41:24 +0200

    creation class child list selection

---
+++ Controler/ChildListSelection.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package Controler;
+
+/**
+ *Controller permettant d'ecouter une  ChildListView dans la vue professeur d'évaluation des eleves.
+ *Ce contrler permet de  recuperer un et un seul eleve.
+ *A partir de cet élève elle permet de generer la vue professeur de la liste des exercices (ExerciseSelectionView)
+ *
+ */
+public class ChildListSelection {
+
+}
