60487916322e87d80f303ef80534e5d461e02350
Wassim <wassim.kerkri@gmail.com>
2017-03-31 14:49:09 +0200

    Controlers + Views

---
+++ Controler/AssaysListSelection.java
@@ -0,0 +1,7 @@
+package Controler;
+
+/**
+ * Created by wassim on 31/03/2017.
+ */
+public class AssaysListSelection {
+}
---
+++ View/ExerciseEditingView.java
@@ -0,0 +1,7 @@
+package View;
+
+/**
+ * Created by wassim on 31/03/2017.
+ */
+public class ExerciseEditingView {
+}
