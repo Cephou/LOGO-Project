10e25362a8d1cb416812c687a5d0b7515318e308
Severin <lhommeletseverin@gmail.com>
2017-03-31 14:45:10 +0200

    ExerciseEvaluationView

---
+++ View/ExerciseEvaluationView.java
@@ -0,0 +1,11 @@
+package View;
+
+// Affiche le nom de l'élève.
+// Affiche le model de l'exercice, la réponse de l'élève (suite d'actions contenu dans l'élève). 
+// Appel de AssayListView qui affiche la liste des essais
+// création de radio button qui permettent de cocher Acquis / en cours / non acquis 
+// JTextField qui permet d'entrer un commentaire
+// Butoon valider qui enregistre tout
+public class ExerciseEvaluationView {
+
+}
