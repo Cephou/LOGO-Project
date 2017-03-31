1085e135ca7029352a571d375995c70530abd26d
Wassim <wassim.kerkri@gmail.com>
2017-03-31 14:38:46 +0200

    ExerciseVisualizationView + AssaysListView

---
+++ View/AssaysListView.java
@@ -0,0 +1,13 @@
+package View;
+
+/**
+ * Cette classe permet de créer une liste de tentatives.
+ */
+public class AssaysListView {
+
+    /**
+     * Constructeur de la classe AssaysListView.
+     */
+    public AssaysListView() {
+    }
+}
