fa6a75cc4082006cba506fe66e56bcc0c74d9866
Severin <lhommeletseverin@gmail.com>
2017-04-03 11:33:22 +0200

    connect

--- Database/DatabaseLoader.java
+++ Database/DatabaseLoader.java
@@ -1,30 +1,46 @@
 package Database;
+import java.sql.*;
 
 // Chargement des données contenu dans la base de données afin de les utiliser dans l'application
 
 public class DatabaseLoader {
+	 
+	/**
+	 *
+	 * @author sqlitetutorial.net
+	 */
 
-	/*
-	 * Exemple de syntaxe de connexion de BDD JDBC/SQLite :
-	 * 
-	 * 
-	 * import java.sql.*;
-
-	public class SQLiteJDBC
-	{
-	  public static void main( String args[] )
-	  {
-	    Connection c = null;
-	    try {
-	      Class.forName("org.sqlite.JDBC");
-	      c = DriverManager.getConnection("jdbc:sqlite:test.db");
-	    } catch ( Exception e ) {
-	      System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-	      System.exit(0);
+	     /**
+	     * Connect to a sample database
+	     */
+	    public static void connect() {
+	        Connection conn = null;
+	        try {
+	            // db parameters
+	            String url = "jdbc:sqlite:C:/sqlite/db/chinook.db";
+	            // create a connection to the database
+	            conn = DriverManager.getConnection(url);
+	            
+	            System.out.println("Connection to SQLite has been established.");
+	            
+	        } catch (SQLException e) {
+	            System.out.println(e.getMessage());
+	        } finally {
+	            try {
+	                if (conn != null) {
+	                    conn.close();
+	                }
+	            } catch (SQLException ex) {
+	                System.out.println(ex.getMessage());
+	            }
+	        }
 	    }
-	    System.out.println("Opened database successfully");
-	  }
-	}*/
-	
-	
+	    /**
+	     * @param args the command line arguments
+	     */
+	    public static void main(String[] args) {
+	        connect();
+	    }
 }
+	
+
