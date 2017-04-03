28e36f4b457f3dea4d189cbf6d8ee93144ab2afa
Alexis A <alexis.amiand@gmail.com>
2017-04-03 10:36:35 +0200

    secure

--- Model/Exercise.java
+++ Model/Exercise.java
@@ -6,47 +6,56 @@ import javax.swing.ImageIcon;
 import Exception.EmptyStringException;
 import Exception.tooLongStringException;
 
-
 /**
- * @author Groupe 7
- * Cette classe décrit l'objet exercice.
- * Un exercice est forcément créé par un professeur.
+ * @author Groupe 7 Cette classe décrit l'objet exercice. Un exercice est
+ *         forcément créé par un professeur.
  */
 
 public class Exercise {
 	private String title;
 	private String instruction;
 	private TortueG turtle;
-	private ImageIcon icon; //L'icône est une miniature du dessin final.
+	private ImageIcon icon; // L'icône est une miniature du dessin final.
 	private Teacher teacher;
 
 	/**
 	 * Constructeur pour la classe Exercice.
-	 * @param title est le titre de l'exercice (taille du string inf à 50 charactères)
-	 * @param instruction est la consigne de l'exercice (taille du string inf à 3000 charactères)
-	 * @param icon est la miniature l'exercice.
-	 * @param teacher est le professeur auteur de l'exercice.
-	 * Un exercice est toujours associé à l'exercice qu'il a écrit.
+	 * 
+	 * @param title
+	 *            est le titre de l'exercice (taille du string inf à 50
+	 *            charactères)
+	 * @param instruction
+	 *            est la consigne de l'exercice (taille du string inf à 3000
+	 *            charactères)
+	 * @param icon
+	 *            est la miniature l'exercice.
+	 * @param teacher
+	 *            est le professeur auteur de l'exercice. Un exercice est
+	 *            toujours associé à l'exercice qu'il a écrit.
+	 * @throws EmptyStringException
+	 * @throws tooLongStringException
 	 */
-	public Exercise(String title, String instruction, ImageIcon icon, Teacher teacher, TortueG turtle) {
+	public Exercise(String title, String instruction, ImageIcon icon,
+			Teacher teacher, TortueG turtle)
+			throws EmptyStringException, tooLongStringException {
 		this.setTitle(title);
 		this.setInstruction(instruction);
-		this.setTurtle(turtle);
-		this.setIcon(icon);
-		this.setTeacher(teacher);
+		this.turtle = turtle;
+		this.icon = icon;
+		this.teacher = teacher;
 	}
 
 	/**
 	 * Permet la saisie d'un titre pour un exercice
-	 * @param title2
-	 * @throws EmptyStringException 
+	 * 
+	 * @param title
+	 * @throws EmptyStringException
 	 */
 	private void setTitle(String title) throws EmptyStringException {
-		if (title.isEmpty()){
+		if (title.isEmpty()) {
 			throw new EmptyStringException();
-		}
-		else this.title = title;
-		
+		} else
+			this.title = title;
 	}
 
 	/**
@@ -64,39 +73,18 @@ public class Exercise {
 	}
 
 	/**
-	 * @param instruction the instruction to set
-	 * @throws EmptyStringException 
-	 * @throws tooLongStringException 
+	 * @param instruction
+	 *            the instruction to set
+	 * @throws EmptyStringException
+	 * @throws tooLongStringException
 	 */
-	public void setInstruction(String instruction) throws EmptyStringException, tooLongStringException {
-		if (instruction.isEmpty()){
+	public void setInstruction(String instruction)
+			throws EmptyStringException, tooLongStringException {
+		if (instruction.isEmpty()) {
 			throw new EmptyStringException();
-		}
-		else if(instruction.length()>500){
+		} else if (instruction.length() > 500) {
 			throw new tooLongStringException();
 		}
-	}
-
-	/**
-	 * @return the icon
-	 */
-	public ImageIcon getIcon() {
-		return icon;
-	}
-
-	/**
-	 * TODO Control ?
-	 * @param icon the icon to set
-	 */
-	public void setIcon(ImageIcon icon) {
-		this.icon = icon;
-	}
-	
-	/**
-	 * @return teacher
-	 */
-	public Teacher getTeacher() {
-		return teacher;
 	}
 
 	/**
@@ -106,14 +94,4 @@ public class Exercise {
 		return turtle;
 	}
 
-	/**
-	 * Permet d'associer une tortue à un exercice
-	 * TODO control ?
-	 * @param turtle the turtle to set
-	 */
-	public void setTurtle(TortueG turtle) {
-		
-		this.turtle = turtle;
-	}
-	
 }
