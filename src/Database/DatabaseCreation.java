0d131e3abfdc28e2da7d6a1848912fffc7024bfb
Severin <lhommeletseverin@gmail.com>
2017-04-04 14:52:02 +0200

    azert

--- src/Database/DatabaseCreation.java
+++ src/Database/DatabaseCreation.java
@@ -17,15 +17,30 @@ public class DatabaseCreation {
 	    public static void databaseCreation() {
 	 
 	        Connection c = null;
+	        Statement stmt = null;
 	 
 	        try {
 	            Class.forName("org.sqlite.JDBC");
 	            c = DriverManager.getConnection("jdbc:sqlite:logo-project.db");
+	            System.out.println("Opened database successfully");
+	            
+	            // Début création de la table
+	            stmt = c.createStatement();
+	            String sql = "CREATE TABLE COMPANY " +
+	                         "(ID INT PRIMARY KEY     NOT NULL," +
+	                         " NAME           TEXT    NOT NULL, " + 
+	                         " AGE            INT     NOT NULL, " + 
+	                         " ADDRESS        CHAR(50), " + 
+	                         " SALARY         REAL)"; 
+	            stmt.executeUpdate(sql);
+	            stmt.close();
+	            c.close();
+
 	          } catch ( Exception e ) {
 	            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
 	            System.exit(0);
 	          }
-	          System.out.println("Opened database successfully");
+	          System.out.println("Table created successfully");          
 	          
 	    }
 	          
