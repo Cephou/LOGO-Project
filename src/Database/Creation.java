6c92a287c34563a5039e19b116f2c82289b60f2c
Severin <lhommeletseverin@gmail.com>
2017-04-04 18:59:58 +0200

    creation and insert marche

--- src/Database/Creation.java
+++ src/Database/Creation.java
@@ -6,29 +6,26 @@ public class Creation {
 
 	public Creation(){
 		database.createTable("drop table if exists ACTION; create table ACTION ( "
-				+ "ID_ACTION            int not null, "
+				+ "ID_ACTION            INTEGER PRIMARY KEY AUTOINCREMENT,"
 				+ "ID_ASSAY             int not null, "
 				+ "COMMAND_ACTION       varchar(50) not null,"
 				+ "D_ACTION             varchar(50) not null,"
 				+ "SPEED_ACTION         varchar(50) not null,"
-				+ "TURTLE_ACTION        varchar(50) not null,"
-				+ "primary key (ID_ACTION));");
-
+				+ "TURTLE_ACTION        varchar(50) not null);");
 		
 		database.createTable("drop table if exists ASSAY;create table ASSAY("
-				+ "ID_ASSAY             int not null,"
+				+ "ID_ASSAY             INTEGER PRIMARY KEY AUTOINCREMENT,"
 				+ "ID_CHILD             int not null,"
 				+ "COMMENT_ASSAY        int not null,"
-				+ "GRADE_ASSAY          varchar(300) not null,"
-				+ "primary key (ID_ASSAY));");
+				+ "GRADE_ASSAY          varchar(300) not null);");
+		
 		database.createTable("drop table if exists CHILD;create table CHILD("
-				+ "ID_CHILD             int not null,"
+				+ "ID_CHILD             INTEGER PRIMARY KEY AUTOINCREMENT, "
 				+ "NAME_CLASS           varchar(50),"
 				+ "FIRSTNAME_CHILD      varchar(50) not null,"
 				+ "LASTNAME_CHILD       varchar(50) not null,"
 				+ "AGE_CHILD            int not null,"
-				+ "PIC_CHILD            longblob not null,"
-				+ "primary key (ID_CHILD));");
+				+ "PIC_CHILD            longblob not null);");
 		
 		database.insertChild("pierre", "HA", 20, "pierre.jpg");
 	}
--- src/Database/Loader.java
+++ src/Database/Loader.java
@@ -16,14 +16,12 @@ public class Loader {
 	
 	private DatabaseObj database = new DatabaseObj();
 	private ArrayList<Child> childList = new ArrayList<Child>();
-	private HashMap<Integer,Integer> indexChild = new HashMap<Integer,Integer>();
 	
 	public Loader() throws SQLException, MalformedURLException, EmptyStringException, NegativeIntegerException, StringSizeException{
 		ResultSet rs = database.selectChildren();
 		while(rs.next()){
 			Child  child = new Child(rs.getString("Firstname_Child"),rs.getString("Lastname_Child"),rs.getInt("Age_Child"),new URL(rs.getString("Pic_Child")));
-			childList.add(child);
-			indexChild.put(childList.indexOf(child), rs.getInt("ID_Child"));			
+			childList.add(child);		
 		}
 	}
 }
